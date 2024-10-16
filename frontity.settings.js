75c591e6df6a383ecd8f39f195800482ef0dd09f
Boursault <benjamin.boursault62@gmail.com>
2024-10-16 14:51:24 +0200

    wp url

--- frontity.settings.js
+++ frontity.settings.js
@@ -15,7 +15,7 @@ const settings = {
       "name": "@frontity/wp-source",
       "state": {
         "source": {
-          "url": "http://api.salon-bureau.fr",
+          "url": "http://lwrgkdy.cluster027.hosting.ovh.net/",
           "homepage": "/"
         }
       }
